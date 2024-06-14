9675b9f50662961694bb060de50c35373a2ba8ef
z-jxy <zjxy.00@gmail.com>
2024-06-14 05:30:17 -0400

    wip: check for edge cases

--- src/pkl/serializer.rs
+++ src/pkl/serializer.rs
@@ -3,24 +3,10 @@ use std::collections::BTreeMap;
 use super::{ObjectMember, PklMod, PklValue};
 
 pub trait PklSerialize {
-    // #[deprecated]
-    // fn serialize_json(&self) -> anyhow::Result<serde_json::Map<String, serde_json::Value>>;
-
     fn serialize_pkl_ast(self) -> anyhow::Result<BTreeMap<String, PklValue>>;
 }
 
 impl PklSerialize for Vec<ObjectMember> {
-    // fn serialize_json(&self) -> anyhow::Result<serde_json::Map<String, serde_json::Value>> {
-    //     let mut json_object = serde_json::Map::new();
-
-    //     for member in self.iter() {
-    //         let (k, v) = member.to_pkl_value()?;
-    //         json_object.insert(k, serde_json::to_value(v)?);
-    //     }
-
-    //     Ok(json_object)
-    // }
-
     fn serialize_pkl_ast(self) -> anyhow::Result<BTreeMap<String, PklValue>> {
         let mut pkl_object = BTreeMap::new();
 
@@ -34,20 +20,6 @@ impl PklSerialize for Vec<ObjectMember> {
 }
 
 impl PklSerialize for PklMod {
-    // fn serialize_json(&self) -> anyhow::Result<serde_json::Map<String, serde_json::Value>> {
-    //     let mut json_object = serde_json::Map::new();
-    //     // let mut obj = BTreeMap::new();
-
-    //     for member in self.members.iter() {
-    //         let (k, v) = member.to_pkl_value()?;
-    //         // obj.insert(k, v);
-    //         json_object.insert(k, serde_json::to_value(v)?);
-    //         // json_object.insert(k, v);
-    //     }
-
-    //     Ok(json_object)
-    // }
-
     /// serializes the module into a btree map
     fn serialize_pkl_ast(self) -> anyhow::Result<BTreeMap<String, PklValue>> {
         let mut pkl_object = BTreeMap::new();

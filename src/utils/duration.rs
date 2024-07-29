5b6c72b83457765afb4aa46917dfe31787fe2566
z-jxy <107861121+z-jxy@users.noreply.github.com>
2024-07-28 23:22:56 -0400

    Remove duration constructors feature (#15)
    * impl duration constructors
    
    * remove feature

---
+++ src/utils/duration.rs
@@ -0,0 +1,44 @@
+use std::time::Duration;
+
+#[cfg(feature = "trace")]
+use tracing::debug;
+
+use crate::utils::macros::_trace;
+
+// work around to avoid requiring unstable feature `duration_constructors` https://github.com/rust-lang/rust/issues/120301
+
+const SECS_PER_MINUTE: u64 = 60;
+const MINS_PER_HOUR: u64 = 60;
+const HOURS_PER_DAY: u64 = 24;
+
+#[inline]
+pub const fn from_days(days: u64) -> Option<Duration> {
+    if days > u64::MAX / (SECS_PER_MINUTE * MINS_PER_HOUR * HOURS_PER_DAY) {
+        _trace!("overflow in Duration::from_days");
+        return None;
+    }
+
+    Some(Duration::from_secs(
+        days * MINS_PER_HOUR * SECS_PER_MINUTE * HOURS_PER_DAY,
+    ))
+}
+
+#[inline]
+pub const fn from_hours(hours: u64) -> Option<Duration> {
+    if hours > u64::MAX / (SECS_PER_MINUTE * MINS_PER_HOUR) {
+        _trace!("overflow in Duration::from_hours");
+        return None;
+    }
+
+    Some(Duration::from_secs(hours * MINS_PER_HOUR * SECS_PER_MINUTE))
+}
+
+#[inline]
+pub const fn from_mins(mins: u64) -> Option<Duration> {
+    if mins > u64::MAX / SECS_PER_MINUTE {
+        _trace!("overflow in Duration::from_mins");
+        return None;
+    }
+
+    Some(Duration::from_secs(mins * SECS_PER_MINUTE))
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,38 @@
+pub(crate) mod duration;
+
+// helper macro to conditionally log trace messages
+mod macros {
+    macro_rules! _trace {
+        ($($arg:tt)*) => {
+            #[cfg(feature = "trace")]
+            tracing::trace!($($arg)*);
+        };
+    }
+
+    macro_rules! _debug {
+        ($($arg:tt)*) => {
+            #[cfg(feature = "trace")]
+            tracing::debug!($($arg)*);
+        };
+    }
+
+    macro_rules! _info {
+        ($($arg:tt)*) => {
+            #[cfg(feature = "trace")]
+            tracing::info!($($arg)*);
+        };
+    }
+
+    macro_rules! _warn {
+        ($($arg:tt)*) => {
+            #[cfg(feature = "trace")]
+            tracing::warn!($($arg)*);
+        };
+    }
+
+    pub(crate) use _trace;
+}
+
 pub fn canonicalize(path: impl AsRef<std::path::Path>) -> std::io::Result<std::path::PathBuf> {
     #[cfg(target_os = "windows")]
     {

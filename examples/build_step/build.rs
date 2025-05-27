d0a33c541fb77cb417e8298a8b44bdfe71e82e54
z-jxy <zjxy.00@gmail.com>
2025-05-27 01:13:10 -0400

    fix doc strings in build_step example

--- examples/build_step/build.rs
+++ examples/build_step/build.rs
@@ -4,18 +4,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     rpkl::build_script::configure()
         .as_enum("Example.mode", &["Dev", "Production"])
         .opaque("Example.mapping")
-        /// derives Clone to the `Database` struct in the module `database`
-        /// ```
-        /// mod database {
-        ///     #[derive(Clone)]
-        ///     pub struct Database;
-        /// }
-        /// ```
-        /// 
-        /// To target the outer struct generated for the module, use `Database` as the identifier.
+        // derives Clone to the `Database` struct in the module `database`
+        // ```
+        // mod database {
+        //     #[derive(Clone)]
+        //     pub struct Database;
+        // }
+        // ```
+        //
+        // To target the outer struct generated for the module, use `Database` as the identifier.
         .type_attribute("database.Database", "#[derive(Clone)]")
         .evaluator_options(EvaluatorOptions::default())
-        .output("generated/mod.rs")
         .codegen(&[
             "../../tests/pkl/example.pkl",
             "../../tests/pkl/database.pkl",
